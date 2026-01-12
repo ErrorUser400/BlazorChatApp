c702d77d16f4f5ab1f6c43e2e6cc5ddb37d6b0ca
BenSam23 <111890508+ErrorUser400@users.noreply.github.com>
2026-01-12 17:48:19 -0600

    add gitignore file

--- BlazorChatApp/BlazorChatApp.Client/obj/Debug/net9.0/BlazorChatApp.Client.AssemblyInfo.cs
+++ BlazorChatApp/BlazorChatApp.Client/obj/Debug/net9.0/BlazorChatApp.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorChatApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101ca76e2ae01631b020a874155e70eafa725d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorChatApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorChatApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HubServer/obj/Debug/net9.0/HubServer.AssemblyInfo.cs
+++ HubServer/obj/Debug/net9.0/HubServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HubServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101ca76e2ae01631b020a874155e70eafa725d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HubServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HubServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
